a7a37d9f872e4a4f7c9a2359919b5656525b6c2e
Sergey Demidov <sergey.k.demidov@gmail.com>
2020-10-14 13:40:08 +0300

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
+// import Vue from "vue";
 import VColoring from "./components/VColoring";
-import VColoringTools from "./components/VColoringTools";
-import VDialogPosition from "./components/VDialogPosition";
+// import VColoringTools from "./components/VColoringTools";
+// import VDialogPosition from "./components/VDialogPosition";
 
-export default {
-  install(Vue) {
-    // Let's register our component globally
-    // https://vuejs.org/v2/guide/components-registration.html
-    Vue.component("v-coloring", VColoring);
-    Vue.component("v-coloring-tools", VColoringTools);
-    Vue.component("v-dialog-position", VDialogPosition);
-  }
-};
+export default VColoring;
